3c6b88e524fb90ac02ee33558eefb107337f5239
tracer-pa-public <tracer@njuics.org>
2023-09-16 16:10:14 +0000

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 16:10:14  up   4:41,  1 user,  load average: 0.30, 0.22, 0.19 > unitime > 6336f4d1587e44fd616cbe74d027f32c5b5210e3

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -113,6 +113,9 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	u32 CF_backup=cpu.eflags.CF;
 	u32 b=alu_add(a,dest,data_size);
 
+	printf("alu_adc: src=%p(%u), dest=%p(%u), of stage1=%u, of stage2=%u, a=%p(%u), b=%p(%u)\n",
+		src,src,dest,dest,OF_backup,cpu.eflags.OF,a,a,b,b);
+
 	cpu.eflags.OF|=OF_backup;
 	cpu.eflags.CF|=CF_backup;
 	return b;
