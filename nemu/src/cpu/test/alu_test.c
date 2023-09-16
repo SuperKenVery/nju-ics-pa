f53f0a8e04663d2af1a5c55bb1c89fd3052e3fbb
tracer-pa-public <tracer@njuics.org>
2023-09-16 16:52:01 +0000

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 16:52:01  up   5:23,  1 user,  load average: 0.25, 0.22, 0.16 > unitime > ea95785f83256cec55dc27c954e256bd196f4c20

--- nemu/src/cpu/test/alu_test.c
+++ nemu/src/cpu/test/alu_test.c
@@ -43,7 +43,7 @@ typedef union {
 	test_eflags.val = res_eflags; \
 	res_asm = res_asm & (0xFFFFFFFF >> (32 - dataSize)); \
 	fflush(stdout); \
-  if(res!=res_asm || cpu.eflags.OF!=test_eflags.OF) printf("res=%p, res_asm=%p, a=%u, b=%u, real of=%d, emu of=%d\n",(void*)res,(void*)res_asm,a,b,test_eflags.OF,cpu.eflags.OF); \
+  if(res!=res_asm || cpu.eflags.OF!=test_eflags.OF) printf("res=%p, res_asm=%p, a=%p, b=%p, real of=%d, emu of=%d\n",(void*)res,(void*)res_asm,(void*)a,(void*)b,test_eflags.OF,cpu.eflags.OF); \
 	assert(res == res_asm); \
 	assert(cpu.eflags.CF == test_eflags.CF); \
 	assert(cpu.eflags.PF == test_eflags.PF); \
