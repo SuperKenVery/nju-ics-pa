82a18972551723c6fb6054fc84c3d5170aa0cc60
tracer-pa-public <tracer@njuics.org>
2023-09-16 14:58:29 +0000

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 14:58:29  up   3:29,  1 user,  load average: 0.31, 0.33, 0.36 > unitime > c2a236fe25e6236cddd067273c24f33e59cd4554

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -107,6 +107,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	u32 a=alu_add(src,dest,data_size);
 	u32 b=alu_add(a,cpu.eflags.CF,data_size);
+	printf("ADC: src=%u, dest=%u, a=%u, b=%u, cf=%u\n",src,dest,a,b,cpu.eflags.CF);
 	return b;
 #endif
 }
