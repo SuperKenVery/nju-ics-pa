ecf844583c93ef15244efc0058dca0a0b152d898
tracer-pa-public <tracer@njuics.org>
2023-09-18 02:43:09 +0000

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 02:43:09  up  14:42,  1 user,  load average: 0.56, 0.34, 0.21 > unitime > 363b1e5a43364964eb6bd74e1769a93c5cb7a815

--- nemu/src/cpu/test/alu_test.c
+++ nemu/src/cpu/test/alu_test.c
@@ -722,7 +722,7 @@ void alu_test_idiv() {
 			rem = alu_imod(b, a);
 			asm ("idivl %%ecx" : "=a" (quo_asm), "=d" (rem_asm) : "a" (aa.low), "d" (aa.high), "c" ((uint32_t)b));
 			fflush(stdout);
-			if(rem!=rem_asm){
+			if(rem!=rem_asm || quo!=quo_asm){
 				printf("(b/a) a=%lld b=%lld quo=%d rem=%d quo_asm=%d rem_asm=%d\n",
 					a,b,quo,rem,quo_asm,rem_asm);
 			}
