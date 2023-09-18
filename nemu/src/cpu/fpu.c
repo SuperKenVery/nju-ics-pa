a3d33b50792bf13565d08c2f0c3bd2429f287a99
tracer-pa-public <tracer@njuics.org>
2023-09-18 12:15:40 +0000

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 12:15:40  up  20:26,  1 user,  load average: 0.12, 0.07, 0.06 > unitime > e99ce48a14deb15956168d12dfc75adf70ab1f1

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -239,7 +239,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	}
 
 	uint32_t exp_res = fb.exponent;
-	printf("fpu.add before normalize: %f (sig_res=%llx exp=%d)\n",
+	printf("fpu.add before normalize: %f (sig_res=%x exp=%d)\n",
 		((double)sig_res) / (1 << (26-be)),
 		sig_res,be
 	);
