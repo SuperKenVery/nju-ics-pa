7c3f11fe44af631bc74e7198a4a1f8092bfa38eb
tracer-pa-public <tracer@njuics.org>
2023-09-18 14:49:23 +0000

    > clean > id 221220099 > user ken > uname Linux lima-intel 6.2.0-32-generic #32-Ubuntu SMP PREEMPT_DYNAMIC Mon Aug 14 10:03:50 UTC 2023 x86_64 GNU/Linux > uptime 14:49:23  up  22:30,  1 user,  load average: 0.33, 0.34, 0.25 > unitime > 6459053fe31b7e2df608ddae1f6f67595bffe9f

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -4,41 +4,43 @@
 #include <stdio.h>
 
 // Copied from https://gist.github.com/ccbrown/9722406
-void _do_hexdump(const void* data, size_t size) {
-	char ascii[17];
-	size_t i, j;
-	ascii[16] = '\0';
-	for (i = 0; i < size; ++i) {
-		printf("%02X ", ((unsigned char*)data)[i]);
-		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-			ascii[i % 16] = ((unsigned char*)data)[i];
-		} else {
-			ascii[i % 16] = '.';
-		}
-		if ((i+1) % 8 == 0 || i+1 == size) {
-			printf(" ");
-			if ((i+1) % 16 == 0) {
-				printf("|  %s \n", ascii);
-			} else if (i+1 == size) {
-				ascii[(i+1) % 16] = '\0';
-				if ((i+1) % 16 <= 8) {
-					printf(" ");
-				}
-				for (j = (i+1) % 16; j < 16; ++j) {
-					printf("   ");
-				}
-				printf("|  %s \n", ascii);
-			}
-		}
-	}
-}
+// void _do_hexdump(const void* data, size_t size) {
+#define hexdump(var)                                                            \
+{                                                                               \
+	char ascii[17];                                                               \
+	size_t i, j;                                                                  \
+	ascii[16] = '\0';                                                             \
+	for (i = 0; i < size; ++i) {                                                  \
+		printf("%02X ", ((unsigned char*)data)[i]);                                 \
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') { \
+			ascii[i % 16] = ((unsigned char*)data)[i];                                \
+		} else {                                                                    \
+			ascii[i % 16] = '.';                                                      \
+		}                                                                           \
+		if ((i+1) % 8 == 0 || i+1 == size) {                                        \
+			printf(" ");                                                              \
+			if ((i+1) % 16 == 0) {                                                    \
+				printf("|  %s \n", ascii);                                              \
+			} else if (i+1 == size) {                                                 \
+				ascii[(i+1) % 16] = '\0';                                               \
+				if ((i+1) % 16 <= 8) {                                                  \
+					printf(" ");                                                          \
+				}                                                                       \
+				for (j = (i+1) % 16; j < 16; ++j) {                                     \
+					printf("   ");                                                        \
+				}                                                                       \
+				printf("|  %s \n", ascii);                                              \
+			}                                                                         \
+		}                                                                           \
+	}                                                                             \
+}                                                                               \
 
 // https://stackoverflow.com/questions/7766115/print-variable-name-in-a-define
-#define hexdump(var) \
-  { \
-    printf("%s:\n",#var); \
-    _do_hexdump(&var,sizeof(var)); \
-  }
+// #define hexdump(var)               \
+//   {                                \
+//     printf("%s:\n",#var);          \
+//     _do_hexdump(&var,sizeof(var)); \
+//   }
 
 /* A convenient function named breakpoint
  * 
