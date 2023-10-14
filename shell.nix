e32d8f4e5cc0c7dd33211443ff0127b01c471ba1
tracer-pa-public <tracer@njuics.org>
2023-10-14 23:08:11 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 23:08:11  up   1:38,  1 user,  load average: 0.46, 0.39, 0.63 > unitime > aa8ddd3f8ce81dbc4f2307deb55abfe8b419f8a4

--- shell.nix
+++ shell.nix
@@ -7,9 +7,6 @@ let
     };
     newpkgs = import <nixpkgs> {
     };
-    newi686pkgs = import <nixpkgs> {
-      system="i686-linux";
-    };
 
 in
   pkgs.stdenv.mkDerivation {
@@ -19,14 +16,7 @@ in
       readline
       SDL
       bear
-      newi686pkgs.glibc
       newpkgs.pwndbg
-      newpkgs.glibc
     ];
 
-    # Nix-ld
-    NIX_LD_LIBRARY_PATH = newi686pkgs.lib.makeLibraryPath [
-      newi686pkgs.stdenv.cc.cc
-    ];
-    NIX_LD = newi686pkgs.lib.fileContents "${newi686pkgs.stdenv.cc}/nix-support/dynamic-linker";
   }
