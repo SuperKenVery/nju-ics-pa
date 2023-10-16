23483d087efb355d6884504f48ae07cb4a209664
tracer-pa-public <tracer@njuics.org>
2023-10-16 17:20:34 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 17:20:34  up 1 day 19:50,  1 user,  load average: 1.06, 0.93, 0.76 > unitime > 3b973bff378fa4cb9c9f5c35643c8e55cc209266

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -5,6 +5,14 @@
 /*
 Put the declarations of `cmp' instructions here.
 */
+make_instr_func(cmp_3c);
+make_instr_func(cmp_3d);
+make_instr_func(cmp_80);
+make_instr_func(cmp_81);
 make_instr_func(cmp_83);
+make_instr_func(cmp_38);
+make_instr_func(cmp_39);
+make_instr_func(cmp_3a);
+make_instr_func(cmp_3b);
 
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,6 +1,7 @@
 #include "cpu-ref/instr_ref.h"
 #include "cpu/alu.h"
 #include "cpu/instr.h"
+#include "cpu/instr_helper.h"
 #include "cpu/modrm.h"
 #include "cpu/operand.h"
 /*
@@ -23,6 +24,21 @@ make_instr_func(cmp_83) {
   operand_read(&imm);
 
   alu_sub(rm.val, imm.val, data_size);
+
+  return len;
+}
+
+make_instr_func(cmp_3b) {
+  int len=1;
+  OPERAND r,rm;
+  r.data_size=data_size;
+  rm.data_size=data_size;
+  len+=modrm_r_rm(eip+len, &r, &rm);
+
+  operand_read(&r);
+  operand_read(&rm);
+
+  alu_sub(r.val, rm.val, data_size);
 
   return len;
 }
