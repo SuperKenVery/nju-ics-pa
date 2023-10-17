866ab07614ac9643296be32acec4ce82647233a3
tracer-pa-public <tracer@njuics.org>
2023-10-17 09:10:46 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 09:10:46  up  10:16,  1 user,  load average: 0.74, 0.58, 0.67 > unitime > 2fe583c4c7c5bca053d1bf488b6231d5a9754c86

--- nemu/include/cpu/instr/inc.h
+++ nemu/include/cpu/instr/inc.h
@@ -1,7 +1,11 @@
 #ifndef __INSTR_INC_H__
 #define __INSTR_INC_H__
+#include <cpu/instr_helper.h>
 /*
 Put the declarations of `inc' instructions here.
 */
+make_instr_func(inc_fe);
+make_instr_func(inc_ff);
+make_instr_func(inc_40);
 
 #endif
--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,4 +1,51 @@
+#include "cpu-ref/instr_ref.h"
 #include "cpu/instr.h"
+#include "cpu/instr_helper.h"
+#include "cpu/modrm.h"
+#include "cpu/operand.h"
 /*
 Put the implementations of `inc' instructions here.
 */
+
+make_instr_func(inc_fe) {
+  int len=1;
+  OPERAND rm;
+  rm.data_size=8;
+
+  len+=modrm_rm(eip+len,&rm);
+
+  operand_read(&rm);
+  rm.val++;
+  operand_write(&rm);
+
+  return len;
+}
+
+make_instr_func(inc_ff) {
+  int len=1;
+  OPERAND rm;
+  rm.data_size=data_size;
+
+  len+=modrm_rm(eip+len, &rm);
+
+  operand_read(&rm);
+  rm.val++;
+  operand_write(&rm);
+
+  return len;
+}
+
+make_instr_func(inc_40) {
+  int len=1;
+  OPERAND r;
+  r.data_size=data_size;
+
+  r.type=OPR_REG;
+  r.addr=opcode%8;
+
+  operand_read(&r);
+  r.val++;
+  operand_write(&r);
+
+  return len;
+}
