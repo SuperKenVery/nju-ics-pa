b16d9f83ca31c80f0ca092d23d80d0a003829ff9
tracer-pa-public <tracer@njuics.org>
2023-10-19 17:39:57 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 17:39:57  up  20:06,  2 users,  load average: 1.62, 1.43, 1.44 > unitime > 9b21cec96895bd336f928f76283a408f207c76e7

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -22,9 +22,10 @@ make_instr_func(cmp_83) {
 
   operand_read(&imm);
 
-  alu_sub(rm.val, imm.val, data_size);
+  alu_sub(rm.val, sign_ext(imm.val,8), data_size);
 
   print_asm_2("cmp", "", len, &rm, &imm);
+  printf("cmp83: len=%d, rm=%d, imm=%d\n",len,rm.val,imm.val);
 
   return len;
 }
