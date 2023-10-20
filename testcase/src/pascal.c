84fb8f1d01fccbdd6175f4f35b0c5783b91fae7a
tracer-pa-public <tracer@njuics.org>
2023-10-20 15:02:34 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 15:02:34  up   5:50,  1 user,  load average: 0.00, 0.03, 0.00 > unitime > e731034443587a3db25edba69acb04d67e7e3ce4

--- testcase/src/pascal.c
+++ testcase/src/pascal.c
@@ -12,31 +12,27 @@ int ans[] = {1, 30, 435, 4060, 27405, 142506, 593775, 2035800, 5852925, 14307150
 int main()
 {
 	int i, j;
-	// for (i = 0; i < N; i++)
-	// {
-	// 	a[i][0] = a[i][i] = 1;
-	// 	nemu_assert(a[i][0] == 1);
-	// 	nemu_assert(a[i][i] == 1);
-	// }
+	for (i = 0; i < N; i++)
+	{
+		a[i][0] = a[i][i] = 1;
+		nemu_assert(a[i][0] == 1);
+		nemu_assert(a[i][i] == 1);
+	}
 
-	// a[0][0]=0;
-	// while(a[0][0]<N) a[0][0]++;
-
-	// HIT_GOOD_TRAP;
 
 	for (i = 2; i < N; i++)
 	{
 		for (j = 1; j < i; j++)
 		{
-			// a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
-			a[i][j]=a[i-1][j];
+			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			// a[i][j]=a[i-1][j];
 		}
 	}
 
-	// for (j = 0; j <= N-1; j++)
-	// {
-	// 	nemu_assert(a[N-1][j] == ans[j]);
-	// }
+	for (j = 0; j <= N-1; j++)
+	{
+		nemu_assert(a[N-1][j] == ans[j]);
+	}
 
 	HIT_GOOD_TRAP;
 
