505899d6283e155ce09a02a94dfc8654899b3749
tracer-pa-public <tracer@njuics.org>
2023-10-22 21:16:00 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 21:16:00  up 1 day  8:59,  2 users,  load average: 1.29, 0.98, 0.93 > unitime > 8ca0e42e118e9b489ad3c50becc116e618351819

--- nemu/include/cpu/instr/call.h
+++ nemu/include/cpu/instr/call.h
@@ -1,7 +1,10 @@
 #ifndef __INSTR_CALL_H__
 #define __INSTR_CALL_H__
+#include <cpu/instr_helper.h>
 /*
 Put the declarations of `call' instructions here.
 */
+
+make_instr_func(call_e8);
 
 #endif
--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,4 +1,20 @@
 #include "cpu/instr.h"
+#include "cpu/instr_helper.h"
+#include "cpu/operand.h"
 /*
 Put the implementations of `call' instructions here.
 */
+
+make_instr_func(call_e8) {
+  int len=1;
+  OPERAND cw;
+  cw.type=OPR_IMM;
+  cw.data_size=16;
+  cw.addr=eip+len;
+  len+=cw.data_size/8;
+
+  operand_read(&cw);
+  cpu.eip=(cpu.eip+cw.val)&0xFFFF;
+
+  return len;
+}
