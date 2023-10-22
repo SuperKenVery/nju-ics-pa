9c984e624d2dc0ece4633d1b57cc3434f66b8aca
tracer-pa-public <tracer@njuics.org>
2023-10-22 23:50:03 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 23:50:03  up 1 day 11:33,  2 users,  load average: 0.62, 0.74, 0.76 > unitime > cda0c9953466fcc294317dcf38af147af4d8b494

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -4,4 +4,12 @@
 Put the declarations of `pop' instructions here.
 */
 
+#include "cpu/instr_helper.h"
+make_instr_func(pop_8f);
+make_instr_func(pop_58);
+make_instr_func(pop_1f);
+make_instr_func(pop_07);
+make_instr_func(pop_17);
+make_instr_func(pop_0f);
+
 #endif
--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -1,4 +1,60 @@
+#include "cpu-ref/instr_ref.h"
 #include "cpu/instr.h"
+#include "cpu/instr_helper.h"
+#include "cpu/modrm.h"
+#include "cpu/operand.h"
+#include "device/port_io.h"
+#include <assert.h>
 /*
 Put the implementations of `pop' instructions here.
 */
+
+u32 pop(int size) {
+  OPERAND esp,stack;
+
+  esp.type=OPR_REG;
+  esp.data_size=32;
+  esp.addr=REG_ESP;
+  operand_read(&esp);
+
+  stack.type=OPR_MEM;
+  stack.data_size=size;
+  stack.addr=esp.val;
+  operand_read(&stack);
+
+  esp.val+=stack.data_size/8;
+  operand_write(&esp);
+
+  return stack.val;
+}
+
+make_instr_func(pop_8f) {
+  int len=1;
+  OPERAND m;
+  m.data_size=data_size;
+  len+=modrm_rm(eip+len, &m);
+  assert(m.type==OPR_MEM);
+
+  m.val=pop(m.data_size);
+  operand_write(&m);
+
+  print_asm_1("pop", "", len, &m);
+
+  return len;
+}
+
+make_instr_func(pop_58) {
+  int len=1;
+  OPERAND r;
+
+  r.type=OPR_REG;
+  r.data_size=data_size;
+  r.addr=opcode%8;
+  
+  r.val=pop(r.data_size);
+  operand_write(&r);
+
+  print_asm_1("pop", "", len, &r);
+
+  return len;
+}
