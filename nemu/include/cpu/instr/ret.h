373da8e65c201a7ab77cdde403bb6791fb299b1f
tracer-pa-public <tracer@njuics.org>
2023-10-22 21:57:46 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.5.6 #1-NixOS SMP PREEMPT_DYNAMIC Fri Oct  6 11:16:31 UTC 2023 x86_64 GNU/Linux > uptime 21:57:46  up 1 day  9:41,  2 users,  load average: 0.74, 0.68, 0.75 > unitime > 682daedac524f1940d26e17e4d7c8429b6c9e82

--- nemu/include/cpu/instr/ret.h
+++ nemu/include/cpu/instr/ret.h
@@ -4,4 +4,7 @@
 Put the declarations of `ret' instructions here.
 */
 
+#include "cpu/instr_helper.h"
+make_instr_func(ret_c3);
+
 #endif
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,4 +1,34 @@
+#include "cpu-ref/instr_ref.h"
 #include "cpu/instr.h"
+#include "cpu/instr_helper.h"
+#include "cpu/operand.h"
 /*
 Put the implementations of `ret' instructions here.
 */
+
+make_instr_func(ret_c3) {
+  int len=1;
+  OPERAND stack,esp;
+
+  // Pop
+  esp.type=OPR_REG;
+  esp.data_size=32;
+  esp.addr=REG_ESP;
+  operand_read(&esp);
+  
+  stack.type=OPR_MEM;
+  stack.data_size=data_size;
+  stack.addr=esp.val;
+  operand_read(&stack);
+
+  esp.val+=stack.data_size/8;
+  operand_write(&esp);
+
+  // Write IP
+  cpu.eip=stack.val;
+  if(stack.data_size==16) cpu.eip=cpu.eip&0xFFFF;
+
+  print_asm_0("ret", "", len);
+
+  return len;
+}
