20c209c38f7bab95d9fa756fba8a814137d25968
tracer-pa-public <tracer@njuics.org>
2023-11-12 01:21:56 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.0 #1-NixOS SMP PREEMPT_DYNAMIC Mon Oct 30 02:31:08 UTC 2023 x86_64 GNU/Linux > uptime 01:21:56  up   3:37,  1 user,  load average: 0.21, 0.26, 0.21 > unitime > 775ac24ba9be8ffb83d4234721b44f7e68b8e9a1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -35,6 +35,9 @@ uint32_t loader()
 	// 	Log("Mem addr %d is %x",i,pos);
 	// }
 	// Log("elf at %p, =%p, elf->e_phoff at %p",&elf,elf,&elf->e_phoff);
+	Log("elf at %p",&elf);
+	Log("elf is %p",elf);
+	Log("phoff at %p",&elf->e_phoff);
 	ph = (Elf32_Phdr*) ((char*)elf + elf->e_phoff);
 	eph = ph + elf->e_phnum;
 	for (; ph < eph; ph++)
