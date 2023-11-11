206062506575f4bfda36191dc094024e719bddf5
tracer-pa-public <tracer@njuics.org>
2023-11-12 00:32:12 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.0 #1-NixOS SMP PREEMPT_DYNAMIC Mon Oct 30 02:31:08 UTC 2023 x86_64 GNU/Linux > uptime 00:32:12  up   2:48,  1 user,  load average: 0.36, 0.25, 0.19 > unitime > 177425dfb0b02851da7542247ed8c18fd77c4327

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,7 @@ uint32_t loader()
 #endif
 
 	/* Load each program segment */
-	Log("About to load ELF, elf is %p",elf);
+	Log("About to load ELF, elf is %x",elf);
 	ph = (void *)elf + elf->e_phoff;
 	Log("AAA");
 	eph = ph + elf->e_phnum;
