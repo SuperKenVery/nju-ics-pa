ee2acd99b372bd827f2673648aeb02ff15fa48ab
tracer-pa-public <tracer@njuics.org>
2023-11-12 00:09:25 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.0 #1-NixOS SMP PREEMPT_DYNAMIC Mon Oct 30 02:31:08 UTC 2023 x86_64 GNU/Linux > uptime 00:09:25  up   2:25,  1 user,  load average: 0.09, 0.17, 0.18 > unitime > 87feaf6867b0c9d1e621d21fa5c6a6616e127c6b

--- shell.nix
+++ shell.nix
@@ -10,6 +10,11 @@ let
     new32pkgs = import <nixpkgs> {
       system="i686-linux";
     };
+    glibc_230 = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/136a26be29a9daa04e5f15ee7694e9e92e5a028c.tar.gz";
+    }) {
+      system="i686-linux";
+    };
 
     my-py-pkgs = ps: with ps; [
       pwntools
