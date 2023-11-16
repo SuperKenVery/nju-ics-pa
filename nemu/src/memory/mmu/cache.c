c0fbb6689c1fad2bb5e8675727457d95323240c5
tracer-pa-public <tracer@njuics.org>
2023-11-16 11:37:24 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.0 #1-NixOS SMP PREEMPT_DYNAMIC Mon Oct 30 02:31:08 UTC 2023 x86_64 GNU/Linux > uptime 11:37:24  up  17:14,  1 user,  load average: 0.63, 0.58, 0.61 > unitime > 3130f9da16567b1613c9eba3501e89ba60ac018e

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -42,7 +42,7 @@ void cache_block_load(cache_block *this, paddr_t mem_addr){
 	printf("eip: 0x%x\tcache_block_load: before memcpy\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 	printf("src=%p, dst=%p, len=%d\n",&hw_mem[mem_addr],this->content,CACHE_BLOCK_SIZE);
 	printf("hw_mem[0x37570] at %p\n", &hw_mem[0x37570]);
-	printf("Cache at %p ~ %p\n",&nemu_cache, &nemu_cache+sizeof(nemu_cache));
+	printf("Cache at %p ~ %p, size = %d\n",&nemu_cache, &nemu_cache+sizeof(nemu_cache),sizeof(nemu_cache));
 	printf("hwmem at %p ~ %p\n",hw_mem,hw_mem+(MEM_SIZE_B));
 	memcpy(this->content, &hw_mem[mem_addr], CACHE_BLOCK_SIZE);
 	printf("eip: 0x%x\tcache_block_load: after memcpy\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
