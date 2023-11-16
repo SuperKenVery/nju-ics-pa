c83befcad72db55a30a3f10a417a8be72792e7d0
tracer-pa-public <tracer@njuics.org>
2023-11-16 11:53:42 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.0 #1-NixOS SMP PREEMPT_DYNAMIC Mon Oct 30 02:31:08 UTC 2023 x86_64 GNU/Linux > uptime 11:53:42  up  17:30,  1 user,  load average: 0.67, 0.66, 0.62 > unitime > 4dac97e0997ccdd2ae2acb592572de6ef8362f4a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,9 +12,7 @@ uint8_t hw_mem[MEM_SIZE_B];
 uint32_t hw_mem_read(paddr_t paddr, size_t len)
 {
 	uint32_t ret = 0;
-	printf("eip: 0x%x\tbefore read\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 	memcpy(&ret, hw_mem + paddr, len);
-	printf("eip: 0x%x\tafter read\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 	return ret;
 }
 
@@ -24,26 +22,17 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 		printf("Invalid memory write operation: write at %u\n",paddr);
 		exit(100);
 	}
-	if(paddr<=0x37570 && paddr+len>=0x37570){
-		printf("hwmem write 37570\n");
-	}
-	printf("eip: 0x%x\tbefore write\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 	memcpy(hw_mem + paddr, &data, len);
-	printf("eip: 0x%x\tafter write\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 }
 
 uint32_t paddr_read(paddr_t paddr, size_t len)
 {
 	uint32_t ret = 0;
-	printf("eip: 0x%x\tEnter paddr_read\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 #ifndef CACHE_ENABLED
-	printf("cache disabled\n");
 	ret = hw_mem_read(paddr, len);
 #else
-	printf("cache enabled\n");
 	ret = cached_read(paddr, len);
 #endif
-	printf("eip: 0x%x\tExit paddr_read\t37570 is %x %x %x %x\n",cpu.eip,hw_mem[0x37570],hw_mem[0x37570+1],hw_mem[0x37570+2],hw_mem[0x37570+3]);
 	return ret;
 }
 
