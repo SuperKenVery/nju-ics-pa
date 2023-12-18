68e7e7e1f702aad2584ac3e83c4db5494c3f6093
tracer-pa-public <tracer@njuics.org>
2023-12-18 23:04:29 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 23:04:29  up 5 days  4:47,  1 user,  load average: 1.91, 2.07, 2.05 > unitime > f82e43cebf45fbcc15c816b2f530f9b1e547e61

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -9,29 +9,26 @@ Put the implementations of `leave' instructions here.
 
 make_instr_func(leave_c9) {
   int len=1;
-  OPERAND sp,bp,stack;
+  OPERAND sp,bp;
 
   // Stack AddrSize should always be 32 in nemu
   sp.data_size=32;
   sp.type=OPR_REG;
   sp.addr=REG_SP;
+
   bp.data_size=32;
   bp.type=OPR_REG;
   bp.addr=REG_BP;
+
+  // sp=bp
   operand_read(&bp);
   sp.val=bp.val;
   operand_write(&sp);
 
-  stack.data_size=data_size;
-  stack.type=OPR_MEM;
-  stack.addr=sp.val;
-  operand_read(&stack);
+  // bp=pop()
   bp.data_size=data_size;
-  bp.val=stack.val;
+  bp.val=pop(32);
   operand_write(&bp);
-
-  sp.val+=stack.data_size/8;
-  operand_write(&sp);
 
   print_asm_0("leave","",len);
 
