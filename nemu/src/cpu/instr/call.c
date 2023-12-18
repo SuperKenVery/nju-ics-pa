ae90927f1024c74112dcccd63337ae227ab00cbe
tracer-pa-public <tracer@njuics.org>
2023-12-18 20:53:24 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 20:53:24  up 5 days  2:36,  1 user,  load average: 0.34, 0.39, 0.45 > unitime > 79841ea1409c281e3bd621d62c625a547af2f4fb

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -9,28 +9,14 @@ Put the implementations of `call' instructions here.
 
 make_instr_func(call_e8) {
   int len=1;
-  OPERAND cw,stack,esp;
+  OPERAND cw;
   cw.type=OPR_IMM;
   cw.data_size=data_size;
   cw.addr=eip+len;
   len+=cw.data_size/8;
   operand_read(&cw);
 
-  esp.type=OPR_REG;
-  esp.data_size=32;
-  esp.addr=REG_ESP;
-  operand_read(&esp);
-  esp.val-=data_size/8;
-  operand_write(&esp);
-
-  stack.type=OPR_MEM;
-  stack.data_size=data_size;
-  stack.addr=esp.val;
-  // https://stackoverflow.com/a/33685182/22556342
-  // `call` pushes the `return address`, so we add len
-  stack.val=cpu.eip+len;
-  // printf("Call: pushing %x onto stack\n",stack.val);
-  operand_write(&stack);
+  push(cpu.eip+len,data_size);
 
   print_asm_1("call","",len,&cw);
 
