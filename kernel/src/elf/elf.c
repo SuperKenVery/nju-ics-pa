34ea5984bdd76e326cac6055c503f50d090857ec
tracer-pa-public <tracer@njuics.org>
2023-12-25 16:49:16 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 16:49:16  up 1 day  6:20,  1 user,  load average: 1.36, 1.33, 1.28 > unitime > fdfcd611d49044f3e27c30953c062e99a39de36b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,6 +37,7 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
+			Log("Segment vaddr=0x%x\n",ph->p_vaddr);
 			mm_malloc(ph->p_vaddr, ph->p_memsz);
 			char *dst=(char*)ph->p_vaddr, *src=((char*)elf)+ph->p_offset;
 			for(int offset=0;offset<ph->p_filesz;offset++){
