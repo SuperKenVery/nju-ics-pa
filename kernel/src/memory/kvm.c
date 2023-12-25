4930b0661edbf246dc08e619dd42e3e8692b7a7c
tracer-pa-public <tracer@njuics.org>
2023-12-25 15:54:54 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 15:54:54  up 1 day  5:25,  1 user,  load average: 1.56, 1.45, 1.61 > unitime > 6e1ddd5761e4e68f53fadbef15e8e487db8558f0

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -38,6 +38,7 @@ void init_page(void)
 	/* make CR3 to be the entry of page directory */
 	cr3.val = 0;
 	cr3.page_directory_base = ((uint32_t)pdir) >> 12;
+	Log("aa");
 	Log("Changing value of cr3: page directory base is 0x%x", cr3.page_directory_base);
 	write_cr3(cr3.val);
 
