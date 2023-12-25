19be32122e35bc33794838a9ad1c847857ecd106
tracer-pa-public <tracer@njuics.org>
2023-12-25 16:55:21 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 16:55:21  up 1 day  6:26,  1 user,  load average: 1.35, 1.51, 1.39 > unitime > 62ba30a71f3dcd7c9404b674affd04cd855e90

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -44,9 +44,6 @@ void init_page(void)
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
-
-	Log("hello");
-	Log("kpdir at %p", kpdir);
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
