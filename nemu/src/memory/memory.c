d22c6d451d35b695bde313df959e34112b6e67aa
tracer-pa-public <tracer@njuics.org>
2023-12-25 09:15:49 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.6.2 #1-NixOS SMP PREEMPT_DYNAMIC Mon Nov 20 10:59:39 UTC 2023 x86_64 GNU/Linux > uptime 09:15:49  up  22:46,  1 user,  load average: 1.02, 0.85, 0.76 > unitime > 6bd61cb9591c116bf3ebaf0a26a51ba874827db

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,7 +12,7 @@
 uint8_t hw_mem[MEM_SIZE_B];
 
 void pinvalid_addr(char *msg, u32 addr){
-	printf("%s: \033[38;2;255;0;0mInvalid memory address \033[38;2;255;255;0m0x%x\033[0m\n",msg,addr);
+	printf("%s: \033[38;2;255;0;0mInvalid memory address \033[38;2;255;255;0m0x%x\033[0m at eip=0x%x\n",msg,addr,cpu.eip);
 	exit(100);
 }
 
