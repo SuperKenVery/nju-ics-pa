20114d3c9ef5defaefee3b7763608a47d6501bde
tracer-pa-public <tracer@njuics.org>
2024-01-23 18:46:16 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 18:46:16  up   9:37,  1 user,  load average: 2.83, 3.11, 2.99 > unitime > b5a7fb7b3dd7f9d6fd548a8629e1598ca03a626

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,12 +2,14 @@
 #include "cpu/instr.h"
 #include "cpu/instr/push.h"
 #include "memory/memory.h"
+#include "nemu.h"
 #include <assert.h>
 
 
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
+	printf("INT %d\n",intr_no);
 	push(cpu.eflags.val, 32);
 	push(cpu.cs.val, 32);
 	push(cpu.eip,32);
@@ -18,6 +20,8 @@ void raise_intr(uint8_t intr_no)
 	GateDesc desc;
 	desc.val[0]=laddr_read(addr, 4);
 	desc.val[1]=laddr_read(addr+4, 4);
+
+	printf("IRQ Handler at 0x%x%x\n",desc.offset_31_16,desc.offset_15_0);
 
 	if(desc.type==0x5){
 		printf("Interrupt to task gate is not implemented. \n");
