389abbb0ab0c777e300574479f16a58d3b8089dc
tracer-pa-public <tracer@njuics.org>
2024-01-24 22:43:44 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 22:43:44  up  12:49,  1 user,  load average: 1.66, 1.94, 1.98 > unitime > b2b81a05814db9f1aeecd033b249e12d58de859

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -51,6 +51,7 @@ uint32_t loader()
 		if (ph->p_type == PT_LOAD)
 		{
 			uint32_t uaddr=mm_malloc(ph->p_vaddr, ph->p_memsz);
+			Log("Allocating 0x%x - 0x%x",ph->p_vaddr,ph->p_vaddr+ph->p_memsz);
 			unsigned char *dst=(unsigned char*)uaddr;
 			ide_read(dst, ELF_OFFSET_IN_DISK+ph->p_offset, ph->p_filesz);
 
