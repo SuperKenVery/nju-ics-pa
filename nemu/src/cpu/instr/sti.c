c3dcd424463b4397a4d092a012847e8c4f6ed3d4
tracer-pa-public <tracer@njuics.org>
2024-01-24 12:30:42 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 12:30:42  up   2:36,  1 user,  load average: 1.86, 2.05, 2.21 > unitime > 1e504a907a79394d532679dbe63956b4a5a44e10

--- nemu/src/cpu/instr/sti.c
+++ nemu/src/cpu/instr/sti.c
@@ -5,9 +5,7 @@ Put the implementations of `sti' instructions here.
 */
 
 make_instr_func(sti){
-  printf("sti: eip=0x%x\n",eip);
   cpu.eflags.IF=1;
 
-  printf("sti: eip became 0x%x\n",eip);
   return 1;
 }
