b728cf3d7f0024f10221d5169d73fb3a6a85ebf8
tracer-pa-public <tracer@njuics.org>
2024-01-24 12:44:29 +0800

    > clean > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 12:44:29  up   2:50,  1 user,  load average: 1.49, 1.73, 1.99 > unitime > aed0a8e9ddc1a1f52820487292b0d85a2e02fde3

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -69,10 +69,6 @@ void exec(uint32_t n)
 	{
 		if(!is_nemu_hlt)
 		{
-			#ifdef IA32_INTR
-			do_intr();
-			#endif
-
 			// printf("eip=0x%x\n", cpu.eip);
 			instr_len = exec_inst();
 			cpu.eip += instr_len;
@@ -111,6 +107,10 @@ void exec(uint32_t n)
 				nemu_state = NEMU_READY;
 				break;
 			}
+
+			#ifdef IA32_INTR
+			do_intr();
+			#endif
 
 		}
 #if defined(HAS_DEVICE_TIMER) || defined(HAS_DEVICE_VGA) || defined(HAS_DEVICE_KEYBOARD) || defined(HAS_DEVICE_AUDIO)
