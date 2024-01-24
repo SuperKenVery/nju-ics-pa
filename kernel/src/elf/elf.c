1f6e8455d02236db06a7e1c5f34e6bb2b0d8a8e6
tracer-pa-public <tracer@njuics.org>
2024-01-24 16:39:56 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 16:39:56  up   6:45,  1 user,  load average: 1.97, 1.85, 2.23 > unitime > d5193c604c9d274135b86c80661c1e14ddc6a90e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -32,6 +32,7 @@ uint32_t loader()
 
 #ifdef HAS_DEVICE_IDE
 	uint8_t buf[4096];
+	Log("hi");
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 	elf = (void *)buf;
 	// Log("ELF loading from hard disk.");
