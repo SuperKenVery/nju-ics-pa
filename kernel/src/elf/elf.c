21b7b49fa0e00b7c20c0a066ee43e42f58e11753
tracer-pa-public <tracer@njuics.org>
2024-01-24 16:49:27 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 16:49:27  up   6:54,  1 user,  load average: 2.09, 2.17, 2.28 > unitime > ae6b9f4e4b069311aaccce289bb0f024a52ea9e3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,11 +48,11 @@ uint32_t loader()
 		if (ph->p_type == PT_LOAD)
 		{
 			Log("Found load seg, disk_addr=0x%x, mem_addr=0x%x, filesz=0x%x, memsz=0x%x",ph->p_offset,ph->p_vaddr,ph->p_filesz,ph->p_memsz);
-			// laddr_parse_t laddr;
-			// laddr.laddr=ph->p_vaddr;
 			uint32_t uaddr=mm_malloc(ph->p_vaddr, ph->p_memsz);
+			Log("done malloc");
 			char *dst=(char*)uaddr, *src=((char*)elf)+ph->p_offset;
 			for(int offset=0;offset<ph->p_filesz;offset++){
+				Log("Loading byte %d",offset);
 				dst[offset]=src[offset];
 			}
 
