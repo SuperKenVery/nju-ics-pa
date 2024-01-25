89566003b1ab4af790f359aa223098f57d79c1b3
tracer-pa-public <tracer@njuics.org>
2024-01-25 13:59:42 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 13:59:42  up   4:16,  1 user,  load average: 1.00, 0.66, 0.59 > unitime > 142983807b4cba95797f88e7ce86555b304d72a4

--- kernel/src/driver/ide/disk.c
+++ kernel/src/driver/ide/disk.c
@@ -77,7 +77,7 @@ void disk_do_read(volatile void *buf, uint32_t sector)
 		if(wbuf[i]!=data){
 			Log("Data corruption: 0x%x -> 0x%x",data,wbuf[i]);
 		}else{
-			Log("Data normal: 0x%x\n",data);
+			Log("Data normal: 0x%x",data);
 		}
 		assert(wbuf[i]==data);
 	}
