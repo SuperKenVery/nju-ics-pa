c698e81ce6fcc24b7bbea037dbc20ed859df1556
tracer-pa-public <tracer@njuics.org>
2024-01-25 11:11:29 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 11:11:29  up   1:28,  1 user,  load average: 0.58, 0.59, 0.58 > unitime > b9cfc6aa433244c96604c542782502b19e508efb

--- kernel/src/driver/ide/disk.c
+++ kernel/src/driver/ide/disk.c
@@ -69,17 +69,18 @@ void disk_do_read(void *buf, uint32_t sector)
 
 #ifndef USE_DMA_READ
 	int i;
+	uint32_t *wbuf=(uint32_t*)buf;
 	printk("kernel disk_do_read sec=%d\n",sector);
 	for (i = 0; i < 512 / sizeof(uint32_t); i++)
 	{
 		uint32_t data = in_long(IDE_PORT_BASE);
 		printk("kernel disk_do_read: got 0x%x\n", data);
-		*(((uint32_t *)buf) + i) = data;
+		wbuf[i] = data;
 		int x=i*4;
 		// Wanted: 0x103a, 0x103b, offset 0x3a, 0x3b
 		if(x>=0x30 && x<=0x3f){
-			printk("kernel disk_do_read inside if: got 0x%x\n", data);
-			uint32_t _data=*(((uint32_t *)buf) + i);
+			printk("kernel disk_do_read inside if: got 0x%x, in buf=0x%x\n", data, wbuf[i]);
+			uint32_t _data=wbuf[i];
 			uint8_t *buf=(uint8_t*)&_data;
 			for(int j=0;j<4;j++){
 				printk("kernel disk_do_read: 0x%x -> 0x%x\n",512*8+x+j,buf[j]);
