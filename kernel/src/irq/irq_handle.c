b0c65a8691fc32c3edfa8dfebff51d2c4b53abdc
tracer-pa-public <tracer@njuics.org>
2024-01-25 21:20:10 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 21:20:10  up   0:31,  1 user,  load average: 1.40, 1.56, 1.43 > unitime > 9ddc866560a4156bbe8c4dad29991d1e8ce769b2

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -51,13 +51,13 @@ void irq_handle(TrapFrame *tf)
 	{
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
-		Log("got irq 1000 + %d",irq_id);
+		if(irq_id!=14) Log("got irq 1000 + %d",irq_id);
 
 		struct IRQ_t *f = handles[irq_id];
 
 		while (f != NULL)
 		{ /* call handlers one by one */
-			Log("Calling routine at %p",f->routine);
+			if(irq_id!=14) Log("Calling routine at %p",f->routine);
 			f->routine();
 			f = f->next;
 		}
