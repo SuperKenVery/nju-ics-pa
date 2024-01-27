2fd66d0ff2ead66151d201d20795707004f23358
tracer-pa-public <tracer@njuics.org>
2024-01-27 11:14:50 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 11:14:50  up   1:28,  1 user,  load average: 1.63, 2.06, 2.57 > unitime > 3cb92fa4cbf31e981c704a33b43076bdaa943aa7

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -75,12 +75,10 @@ void video_mapping_read_test()
 	Log("starting read test, vmem at %p",buf);
 	for (i = 0; i < SCR_SIZE / 4; i++)
 	{
-		uint32_t *target=&buf[i];
-		Log("Checking %p",target);
-		uint32_t x=*target;
-		assert(x==i);
-		// assert(buf[i] == i);
-		Log("Done checking %p",&buf[i]);
+		// uint32_t *target=&buf[i];
+		// uint32_t x=*target;
+		// assert(x==i);
+		assert(buf[i] == i);
 	}
 }
 
