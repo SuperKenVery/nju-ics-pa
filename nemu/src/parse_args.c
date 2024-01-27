89553cc01c9609a6bff6476a3f6ba78d14aad034
tracer-pa-public <tracer@njuics.org>
2024-01-27 12:09:45 +0800

    > compile nemu testcase kernel > id 221220099 > user ken > uname Linux ken-nix 6.7.0 #1-NixOS SMP PREEMPT_DYNAMIC Sun Jan  7 20:18:38 UTC 2024 x86_64 GNU/Linux > uptime 12:09:45  up   2:23,  1 user,  load average: 3.80, 2.75, 2.37 > unitime > ca800f533560e61355c69233d46a07f146bffab5

--- nemu/src/parse_args.c
+++ nemu/src/parse_args.c
@@ -153,6 +153,7 @@ static bool main_arg_game(char *input_stu_id)
 	strcpy(elf_path_score, "./scripts/score_testcases/game");
 	strcpy(stu_id, input_stu_id);
 	// flag_game = true;
+	flag_testcase=true;
 	return true;
 }
 
