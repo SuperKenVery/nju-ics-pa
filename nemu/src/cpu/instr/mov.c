3d03c0d0b1a3ab6f0c31775b553d4ebb4fbb6f05
tracer-pa-public <tracer@njuics.org>
2024-01-28 10:43:42 +0800

    > compile nemu testcase kernel > id 221220099 > user root > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 10:43:42 up 42 min,  1 user,  load average: 0.01, 0.11, 0.16 > unitime 1706409821987 > 56ffd7b22d579dc639f3d5e063c8f8aaf5e6d209

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -32,8 +32,11 @@ make_instr_impl_2op(mov, o, a, b)
 make_instr_impl_2op(mov, o, a, v)
 
 make_instr_func(mov_rm2r_v){
+  bool backup=verbose;
+  verbose=true;
   int result=_mov_rm2r_v(eip, opcode);
   if(eip>=0xc0031af7 && eip<=0xc0031b9a) printf("[%x: R *0x%x] ",eip&0xfffff, opr_src.addr);
+  verbose=backup;
   return result;
 }
 
