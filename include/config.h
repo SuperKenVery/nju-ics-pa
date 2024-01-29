220b59f3f15639c34a9f595d0517b75d654b12e0
tracer-pa-public <tracer@njuics.org>
2024-01-29 17:54:04 +0800

    > compile nemu testcase kernel > id 221220099 > user root > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 17:54:04 up 3 min,  1 user,  load average: 0.19, 0.17, 0.08 > unitime 1706522038994 > 99c15621ac4294dd27781af07a8ded65831cd7be

--- include/config.h
+++ include/config.h
@@ -19,7 +19,7 @@
 //#define HAS_DEVICE_AUDIO     // audio, this is experimental, need to enable when submit pa-4-3
 
 // #define QUICK_START          // Skip ide game load
-#define HAS_DEVICE_GPU
+// #define HAS_DEVICE_GPU
 #define QUICK_DISK
 
 // #define PA_ONLINE
