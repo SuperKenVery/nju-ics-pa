e7bfb59eb05735da8b1e09125cdbc1ac9cd95007
tracer-pa-public <tracer@njuics.org>
2024-01-29 12:17:22 +0800

    > clean > id 221220099 > user root > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 12:17:22 up  2:59,  1 user,  load average: 0.06, 0.05, 0.08 > unitime > d612599c137f5c9e8594782f98406aaf2f23aa98

--- nemu/src/device/dev/gpu.c
+++ nemu/src/device/dev/gpu.c
@@ -9,6 +9,52 @@
 
 #ifdef HAS_DEVICE_GPU
 
+typedef struct GPU_CMD{
+  vaddr_t src_surface,dst_surface,src_rect,dst_rect;
+} GPU_CMD;
+
+typedef struct
+{
+	int16_t x, y;
+	uint16_t w, h;
+} SDL_Rect;
+
+typedef struct
+{
+	uint8_t r, g, b, unused;
+} SDL_Color;
+
+typedef struct
+{
+	int ncolors;
+	SDL_Color *colors;
+} SDL_Palette;
+
+typedef struct
+{
+	SDL_Palette *palette;
+	uint8_t BitsPerPixel;
+	uint8_t BytesPerPixel;
+	uint8_t Rloss, Gloss, Bloss, Aloss;
+	uint8_t Rshift, Gshift, Bshift, Ashift;
+	uint32_t Rmask, Gmask, Bmask, Amask;
+	uint32_t colorkey;
+	uint8_t alpha;
+} SDL_PixelFormat;
+
+typedef struct
+{
+	uint32_t flags;
+	SDL_PixelFormat *format;
+	int w, h;
+	uint16_t pitch;
+	SDL_Rect clip_rect;
+
+	int refcount;
+	uint8_t *pixels;
+
+} SDL_Surface;
+
 void gpu_init(){
   
 }
